b30cb5485865a0e16385fe5378a6330066367fae
Mariya <appleslover@yandex.ru>
2023-01-19 19:00:28 +0300

    TasK_2_4

---
+++ Task _2_4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет кратно ли оно одновременно и 7, и 23
+
+int GetNumber()
+{
+    Console.Write("Введите число: ");
+    return Convert.ToInt32(Console.ReadLine()); 
+}
+
+int number = GetNumber();
+
+if (number % 7 == 0 && number % 23 == 0)
+{
+    Console.WriteLine($"число " + (number) + " кратно и 7, и 23");
+}
+else
+{
+    Console.WriteLine($"число " + (number) + " не кратно ни 7, ни 23");
+}
