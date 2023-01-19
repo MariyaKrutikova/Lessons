95da94c2966c738ab184ed32e784e181b28900bc
Mariya <appleslover@yandex.ru>
2023-01-19 17:57:10 +0300

    TasK_2_3

---
+++ Task_2_3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая на вход принмимает два числа и выводить, является ли второе число кратным первому.
+// Если нет, то выводить остаток от деления
+
+int GetNumber()
+{
+    Console.WriteLine("Введите число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int FirstNumber = GetNumber();
+int SecondNumber = GetNumber();
+
+if (SecondNumber % FirstNumber == 0)
+{
+    Console.WriteLine("кратно");
+}
+else
+{
+    Console.WriteLine($"не кратно, остаток - " + (SecondNumber % FirstNumber));
+}
