37f9a3d779b9911383a14f25f904065ba867d78f
Mariya <appleslover@yandex.ru>
2023-01-19 19:13:51 +0300

    TasK_2_5

---
+++ Task_2_5/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Напишиите программу, которая на вход принимает два числа и проверяет, 
+является ли одно число, квадратом второго */
+
+int GetNewNumber()
+{
+    Console.Write("Введите число: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int FirstNumber = GetNewNumber();
+int SecondNumber = GetNewNumber();
+
+if (FirstNumber*FirstNumber == SecondNumber | SecondNumber*SecondNumber == FirstNumber)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
