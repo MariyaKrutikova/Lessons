fe7130778290c79f1e5d6b428d96d89e074c016c
Mariya <appleslover@yandex.ru>
2023-01-19 16:08:07 +0300

    TasK_2_2

---
+++ Task_2_2/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Написать программу, которая выводи случайное трехзначное число и удаляет вторую цифру этого числа
+456 -> 46
+783 -> 73
+918 -> 98
+*/
+
+//Метод, который вывод случайное трехзначное число
+int GetNewNumber(int leftBount, int rightBount)
+{
+    int result = 0;
+    Random rnd = new Random();
+    result = rnd.Next(leftBount, rightBount);
+    return result;
+}
+
+//Метод, который удаляет вторую цифру в трехзначном числе
+
+int DelitSecondNumber(int number)
+{
+    return (number / 100)*10 + (number % 10);
+}
+
+int number = GetNewNumber(100, 999);
+Console.WriteLine(number);
+
+int NewNumber = DelitSecondNumber(number);
+Console.WriteLine(NewNumber);
