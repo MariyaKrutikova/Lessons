306ef39ea7270f4e82175bea333667180b07856a
Mariya <appleslover@yandex.ru>
2023-01-26 13:30:45 +0300

    Task_4_1_SumOfDigitsInNumbers

---
+++ Task_4_1_SumOfDigitsInNumbers/Program.cs
@@ -0,0 +1,39 @@
+﻿/* Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+7 -> 28
+4 -> 10
+8 -> 36*/
+
+int GetNumber(string message)
+{
+    int result = 0;
+    while(true)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out result) & result > 1)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Некорректные данные. Повторите ввод");
+        }
+    }
+    return result;
+}
+
+
+int GetSum(int number)
+{
+    int i = 0;
+    int Sum = 0;
+    for (i = 0; i <= number; i++)
+    {
+        Sum = Sum + i;
+    }
+   return Sum;
+ 
+}
+
+int number = GetNumber("Введите число: ");
+int Sum = GetSum(number);
+Console.WriteLine(Sum);
