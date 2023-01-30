367e07509ad4ae916d6ea47d82c5175ebdc55045
Mariya <appleslover@yandex.ru>
2023-01-30 18:00:53 +0300

    Task_5_3_FindElementinArray

---
+++ Task_5_3_FindElementinArray/Program.cs
@@ -0,0 +1,45 @@
+﻿/*Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+4; массив [6, 7, 19, 345, 3] -> нет
+-3; массив [6, 7, 19, 345, 3] -> да*/
+
+int[] GetNewArray(int demention)
+{
+    int[] array = new int[demention];
+    Random rnd = new Random();
+    for (int i = 0; i < demention; i++)
+    {
+        array[i] = rnd.Next(0,10);
+    }
+    return array;
+}
+
+// void PrintArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write($"{array[i]}  ");
+//     }
+// }
+
+    
+bool FindNumber(int find, int[] array)
+{
+    bool result = false;
+    for (int i = 0; i < array.Length; i++)
+    {
+       if (find == array[i])
+       {
+            result = true;
+            break;
+       } 
+    } 
+    return result;
+}
+
+int[] myArray = GetNewArray(15);
+Console.WriteLine("Что ищем?");
+int find = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(string.Join(", ", myArray));
+bool a = FindNumber(find, myArray);
+if (a) Console.WriteLine("Число есть в массиве");
+else Console.WriteLine("Числа нет в массиве");
