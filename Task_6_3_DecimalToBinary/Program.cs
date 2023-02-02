6a151ba71c6e85d29f8c2536807f231f6b332493
Mariya <appleslover@yandex.ru>
2023-02-02 17:49:07 +0300

    Task_6_3_DecimalToBinary

---
+++ Task_6_3_DecimalToBinary/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+45 -> 101101
+3 -> 11
+2 -> 10*/
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+string number_str = "";
+
+while (number > 0)
+{
+number_str = number_str + number % 2 ;
+number = number / 2 ;
+}
+string result = new string(number_str.Reverse().ToArray());
+Console.WriteLine(result);
