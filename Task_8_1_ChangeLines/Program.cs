5bc4bd0bfb3209afa3bb5cdce75b9584ad77a0e9
Mariya <appleslover@yandex.ru>
2023-02-10 17:01:05 +0300

    Task_8_1_ChangeLines

---
+++ Task_8_1_ChangeLines/Program.cs
@@ -0,0 +1,67 @@
+﻿/*Задача 53: Задайте двумерный массив. Напишите программу, 
+которая поменяет местами первую и последнюю строку массива.*/
+
+int GetNumber(string message)
+{
+    int result = 0;
+    while(true)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out result) & result > 0)
+            break;
+        else
+        {
+            Console.WriteLine("Некорректные данные. Повторите ввод.");
+        }    
+    }
+    return result;
+}
+
+int [,] InitMatrix(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for ( int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = rnd.Next(0,10);
+        }
+    }
+    return matrix;
+}  
+
+int[,] PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                Console.Write($"{matrix[i,j]}  ");
+            }
+            Console.WriteLine();
+        }
+    return matrix;
+}
+
+int[,] ChangeMatrix(int[,] matrix)
+{    
+    for (int j =0; j < matrix.GetLength(1); j++)
+    {
+        int remamber = matrix[0, j]; 
+        matrix[0, j] = matrix[matrix.GetLength(0)-1, j];
+        matrix[matrix.GetLength(0)-1, j] = remamber;
+    }
+    return matrix;
+}
+
+int rows = GetNumber("Задайте количество строк: ");
+int columns = GetNumber("Задайте количество столбцов: ");
+
+int[,] matrix = InitMatrix(rows, columns);
+PrintMatrix(matrix);
+
+Console.WriteLine();
+
+int[,] changematrix = ChangeMatrix(matrix);
+PrintMatrix(changematrix);
