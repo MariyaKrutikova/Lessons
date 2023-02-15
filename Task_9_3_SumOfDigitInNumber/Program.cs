6de711bc7df33a660a2732343864d06d2dd5b215
Mariya <appleslover@yandex.ru>
2023-02-15 17:14:22 +0300

    Task_9_3_4

---
+++ Task_9_3_SumOfDigitInNumber/Program.cs
@@ -0,0 +1,38 @@
+﻿/*Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+453 -> 12
+45 -> 9*/
+
+int GetNumber(string message)
+{
+    int result = 0;
+    while(true)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out result) & result > 0)
+            break;
+        else
+        {
+            Console.WriteLine("Некорректные данные. Повторите ввод.");
+        }    
+    }
+    return result;
+}
+
+int DigitSumRecurtion(int number)
+{
+    // if (number == 0)
+    //     return number;
+    // else     
+    //     return number % 10 + DigitSumRecurtion(number / 10);
+
+    if (number != 0 )
+    {
+        int sum = number % 10 + DigitSumRecurtion(number);
+        number = number / 10;
+    }
+    return sum;
+}
+
+int number = GetNumber("Введите число");
+int sum = DigitSumRecurtion(number);
+Console.WriteLine(sum);
---
+++ Task_9_4_PowerOfNumber/Program.cs
@@ -0,0 +1,37 @@
+﻿/*Задача 69: Напишите программу, которая на вход принимает два числа A и B, 
+и возводит число А в целую степень B с помощью рекурсии.
+A = 3; B = 5 -> 243 (3⁵)
+A = 2; B = 3 -> 8*/
+
+int GetNumber(string message)
+{
+    int result = 0;
+    while(true)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out result) & result > 0)
+            break;
+        else
+        {
+            Console.WriteLine("Некорректные данные. Повторите ввод.");
+        }    
+    }
+    return result;
+}
+
+int FindPower(int A, int B)
+{
+    if (B == 0)
+        return A;
+    else
+    {
+        return A * FindPower(A, B-1);
+    }
+}
+
+
+int A = GetNumber("Введите первое число");
+int B = GetNumber("Введите второе число");
+
+int result = FindPower(A, B);
+Console.WriteLine(result);
