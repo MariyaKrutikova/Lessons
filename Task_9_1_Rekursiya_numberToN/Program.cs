7483a42f689b1b13faa44cba29ed5d78b053c737
Mariya <appleslover@yandex.ru>
2023-02-15 13:27:45 +0300

    Task_9_1_Rekursiya_numberToN

---
+++ Task_9_1_Rekursiya_numberToN/Program.cs
@@ -0,0 +1,34 @@
+﻿/* Задача 63. Задайте N. Напишите программу, которая выведет все натуральные числа от 1 до N*/
+
+int GetNumber(string message)
+{
+    int result = 0;
+    while(true)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out result) & result > 0)
+            break;
+        else
+        {
+            Console.WriteLine("Некорректные данные. Повторите ввод.");
+        }    
+    }
+    return result;
+}
+
+
+void Recursion(int N, int number)
+{
+     if (number <= N) 
+    {
+        Console.Write($"{number} ");
+        number++;
+        Recursion(N, number);
+    }
+
+}
+
+int N = GetNumber("Введите число");
+Recursion(N, 0);
+
+
